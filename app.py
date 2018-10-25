416cbf278a1282e11b5fa612593672d1dd9f8848
krood20 <krood20@gwmail.gwu.edu>
2018-10-24 23:32:36 -0400

    added a signup page, for some reason the toggle menu broke

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run()
