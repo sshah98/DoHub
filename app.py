d10fb96e6e55ff970f3208564a4301370e2d5708
sshah98 <shahsuraj261@gmail.com>
2018-10-24 22:28:25 -0400

    added database and migrations for database

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request, session, flash, Markup
+from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+import os
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# database = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='allow')
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///scorpio"
+
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
---
+++ models.py
@@ -0,0 +1,14 @@
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    email = db.Column(db.String(), unique=True)
+    password = db.Column(db.String())
+    kitchen = db.Column(db.String())
+
+    def __repr__(self):
+        return 'Email {}>'.format(self.email)
