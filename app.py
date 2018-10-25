3f1cb224066e6bbc47ad5c54797f4f143dd3fc3d
sshah98 <shahsuraj261@gmail.com>
2018-10-24 23:29:50 -0400

    updated app to account for heroku database

--- app.py
+++ app.py
@@ -3,24 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 import os
 
+
+HEROKU_DB = "postgres://utjukngdvaagnt:9e3ad063a636e4cc1ed33e0cdca2ba858daf3040ee4df5e4ed21132f4c2c82f9@ec2-50-17-194-186.compute-1.amazonaws.com:5432/d47t7c5tma2rd5"
+
+LOCAL_DB = "postgres:///scorpio"
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-# database = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='allow')
-
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgres:///scorpio"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://utjukngdvaagnt:9e3ad063a636e4cc1ed33e0cdca2ba858daf3040ee4df5e4ed21132f4c2c82f9@ec2-50-17-194-186.compute-1.amazonaws.com:5432/d47t7c5tma2rd5"
-
-
-# database = psycopg2.connect("postgres:///scorpio", sslmode='allow')
-# database = psycopg2.connect
-
-database = psycopg2.connect(
-    'postgres://utjukngdvaagnt:9e3ad063a636e4cc1ed33e0cdca2ba858daf3040ee4df5e4ed21132f4c2c82f9@ec2-50-17-194-186.compute-1.amazonaws.com:5432/d47t7c5tma2rd5')
-
+app.config['SQLALCHEMY_DATABASE_URI'] = LOCAL_DB
+database = psycopg2.connect(LOCAL_DB, sslmode='allow')
 db = SQLAlchemy(app)
 from models import *
-
 
 @app.route('/')
 def hello():
