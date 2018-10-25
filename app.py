1e6b41096cbe878aa6ab7311abad53cdc71e36b8
Benjamin Fernandez <fernandezb@gwu.edu>
2018-10-24 22:59:12 -0400

    added in initial implementation for magic calendar

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/calendar')
+def calendar():
+    return render_template('json.html')
 
 if __name__ == '__main__':
     app.run()
