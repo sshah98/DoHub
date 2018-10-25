7aed69dad00eb8e52e01bfd2197aa95267f0273f
sshah98 <shahsuraj261@gmail.com>
2018-10-24 23:22:30 -0400

    updating url name

--- app.py
+++ app.py
@@ -8,10 +8,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 # database = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='allow')
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres:///scorpio"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgres:///scorpio"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://utjukngdvaagnt:9e3ad063a636e4cc1ed33e0cdca2ba858daf3040ee4df5e4ed21132f4c2c82f9@ec2-50-17-194-186.compute-1.amazonaws.com:5432/d47t7c5tma2rd5"
 
-database = psycopg2.connect("postgres:///scorpio", sslmode='allow')
+
+# database = psycopg2.connect("postgres:///scorpio", sslmode='allow')
 # database = psycopg2.connect
+
+database = psycopg2.connect(
+    'postgres://utjukngdvaagnt:9e3ad063a636e4cc1ed33e0cdca2ba858daf3040ee4df5e4ed21132f4c2c82f9@ec2-50-17-194-186.compute-1.amazonaws.com:5432/d47t7c5tma2rd5')
 
 db = SQLAlchemy(app)
 from models import *
